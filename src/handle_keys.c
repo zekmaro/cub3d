f3295628ae0a475314c76c4c9ec059f80355cf84
Istvan Beregszaszi <iberegsz@c3r2p3.42vienna.com>
2024-08-29 23:28:35 +0200

    Norminette handle_keys.c

--- src/handle_keys.c
+++ src/handle_keys.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   handle_keys.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anarama <anarama@student.42.fr>            +#+  +:+       +#+        */
+/*   By: iberegsz <iberegsz@student.42vienna.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/21 23:55:11 by andrejarama       #+#    #+#             */
-/*   Updated: 2024/08/29 22:28:23 by anarama          ###   ########.fr       */
+/*   Updated: 2024/08/29 23:27:29 by iberegsz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,32 @@
 
 void	move_player(t_vars *vars, int move_y, int move_x)
 {
-	int x;
-	int y;
+	int	x;
+	int	y;
 
 	x = (vars->player->center_x + move_x) / vars->unit_size;
 	y = (vars->player->center_y + move_y) / vars->unit_size;
 	if (can_move(vars, y, x))
 	{
 		vars->player->y += move_y;
-		vars->player->x += move_x;	
+		vars->player->x += move_x;
 	}
 }
 
-int mouse_move(int x, int y, t_vars *vars)
+int	mouse_move(int x, int y, t_vars *vars)
 {
 	(void)y;
 	vars->player->angle = (x / 360);
 	draw_map(vars);
 	mlx_put_image_to_window(vars->mlx->mlx, vars->mlx->win,
-			vars->image->mlx_img, 0, 0);
-    return (0);
+		vars->image->mlx_img, 0, 0);
+	return (0);
 }
 
 void	check_move_player(int keycode, t_vars *vars)
 {
-	int move_x;
-	int move_y;
+	int	move_x;
+	int	move_y;
 
 	move_x = 8;
 	move_y = 8;
@@ -63,7 +63,6 @@ void	check_move_player(int keycode, t_vars *vars)
 		vars->player->angle -= M_PI / 6;
 	else if (keycode == KEY_RIGHT)
 		vars->player->angle += M_PI / 6;
-		
 }
 
 void	handle_key(int keycode, t_vars *vars)
