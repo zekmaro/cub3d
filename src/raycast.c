1cca1d83d014383705175cacd9a367b4693749f2
Andrey Arama <anarama@c2r7p6.42vienna.com>
2024-09-03 19:25:53 +0200

    im legend

--- src/raycast.c
+++ src/raycast.c
@@ -65,6 +65,25 @@ void	get_ray_target_coords(t_vars *vars)
 	}
 }
 
+void	custom_setup_ray(t_vars *vars, double ray_x, double ray_y)
+{
+	vars->ray->distance_to_wall = sqrt(pow(ray_x \
+		- vars->player->x, 2) + pow(ray_y - vars->player->y, 2));
+	double dynamic_offset = (vars->mlx->window_height * 10 / vars->ray->distance_to_wall);
+	vars->ray->line_height = (int)(vars->mlx->window_height \
+		* vars->unit_size / vars->ray->distance_to_wall);
+	vars->ray->draw_start = -vars->ray->line_height / 2 \
+		+ vars->mlx->window_height / 2;
+	vars->ray->draw_start += (44 + dynamic_offset);
+	if (vars->ray->draw_start < 0)
+		vars->ray->draw_start = 0;
+	vars->ray->draw_end = vars->ray->line_height / 2 \
+		+ vars->mlx->window_height / 2;
+	vars->ray->draw_end += (44 + dynamic_offset);
+	if (vars->ray->draw_end >= vars->mlx->window_height)
+		vars->ray->draw_end = vars->mlx->window_height - 1;
+}
+
 void	setup_ray(t_vars *vars, double ray_x, double ray_y)
 {
 	vars->ray->distance_to_wall = sqrt(pow(ray_x \
@@ -132,14 +151,16 @@ void	draw_ray_column(t_vars *vars, int ray_id, t_tex_typ texture_index)
 		}
 		else
 		{
-			put_pixel_to_image(vars, vars->ray->ray_monster_x, vars->ray->ray_monster_y, RED);
 			tmp = (t_img *)vars->animated_sprite->frames[vars->animated_sprite->current_frame];
-			if (tex_y <= 63 && tex_x <= 63)
+			if (tex_y <= vars->unit_size && tex_x <= vars->unit_size)
 			{
-				setup_ray(vars, vars->ray->ray_monster_x, vars->ray->ray_monster_y);
+				custom_setup_ray(vars, vars->ray->ray_monster_x, vars->ray->ray_monster_y);
 				tex_y = (int)((y - vars->ray->draw_start) * vars->unit_size \
 					/ vars->ray->line_height);
 				tex_x = (int)(vars->ray->ray_monster_x) % 44;
+				//printf("%d %d %d\n", y, vars->ray->draw_start, y - vars->ray->draw_start);
+				if (tex_y < 0)
+					tex_y = 0;
 				color = get_texture_color(tmp, tex_x, tex_y);
 				if (color == -1)
 				{
@@ -147,7 +168,7 @@ void	draw_ray_column(t_vars *vars, int ray_id, t_tex_typ texture_index)
 					get_texture_coords(vars, texture_index, &tex_x);
 					tex_y = (int)((y - vars->ray->draw_start) * vars->unit_size \
 						/ vars->ray->line_height);
-					if (tex_y <= 63)
+					if (tex_y <= vars->unit_size)
 					{
 						color = get_texture_color(vars->textures[texture_index], tex_x, tex_y);
 					}
