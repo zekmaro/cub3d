1b0a202a4ad3516834c36bf8f45c876b7247fa43
Andrey Arama <anarama@c2r7p1.42vienna.com>
2024-09-06 19:48:46 +0200

    added imp pain sound

--- src/cub3d.c
+++ src/cub3d.c
@@ -72,7 +72,8 @@ int	main_loop_hook(t_vars *vars)
 	long imp_elapsed_time = get_elapsed_time(&vars->imp->time0, &vars->imp->time1);
 	if (imp_elapsed_time > 200)
 	{
-		if (vars->imp->current_animation == vars->imp->death_animation && vars->imp->current_animation->current_frame == 4)
+		if (vars->imp->current_animation == vars->imp->death_animation
+			&& vars->imp->current_animation->current_frame == vars->imp->current_animation->frame_count - 1)
 		{
 			//printf("%d \n",vars->imp->current_animation->frame_count);
 			vars->imp->is_dead = 1;
