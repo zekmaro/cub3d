56abcedc9143b10cb607b9750d9c4e5db7645ceb
Istvan Beregszaszi <iberegsz@c2r7p5.42vienna.com>
2024-09-10 01:04:20 +0200

    Extracting is_entity from map_utils

---
+++ src/is_entity.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   is_entity.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iberegsz <iberegsz@student.42vienna.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/10 01:00:29 by iberegsz          #+#    #+#             */
+/*   Updated: 2024/09/10 01:03:02 by iberegsz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../cub3d.h"
+
+int	is_player(t_vars *vars, int y, int x)
+{
+	return (abs(y - vars->player->center_y) < 10 \
+		&& abs(x - vars->player->center_x) < 10);
+}
+
+int	is_enemy(t_enemy *enemy, int y, int x)
+{
+	return (abs(y - enemy->center_y) < 20 && abs(x - enemy->center_x) < 20);
+}
+
+int	is_imp(t_vars *vars, int y, int x)
+{
+	return (abs(y - vars->imp->center_y) < 40 \
+		&& abs(x - vars->imp->center_x) < 40);
+}
+
+int	is_caco(t_vars *vars, int y, int x)
+{
+	return (abs(y - vars->caco->center_y) < 40 \
+		&& abs(x - vars->caco->center_x) < 40);
+}
+
+int	is_wall(t_vars *vars, int y, int x)
+{
+	return (vars->map->grid[y / vars->unit_size][x \
+		/ vars->unit_size] == '1');
+}
