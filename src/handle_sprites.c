8d7b494cb5266f72ab49262e6cf87bdfc49e28f0
Istvan Beregszaszi <iberegsz@c2r7p5.42vienna.com>
2024-09-10 15:13:37 +0200

    Rename sprites into handle_sprites

--- src/handle_sprites.c
+++ src/handle_sprites.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   sprites.c                                          :+:      :+:    :+:   */
+/*   handle_sprites.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: iberegsz <iberegsz@student.42vienna.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/01 01:03:38 by iberegsz          #+#    #+#             */
-/*   Updated: 2024/09/10 13:16:03 by iberegsz         ###   ########.fr       */
+/*   Updated: 2024/09/10 15:11:14 by iberegsz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,28 +52,6 @@ void	load_animated_sprite(t_vars *vars, t_img *sprite, \
 void	update_sprite_frame(t_img *sprite)
 {
 	sprite->current_frame = (sprite->current_frame + 1) % sprite->frame_count;
-}
-
-// void	put_enemy_on_screen(t_vars *vars)
-// {
-// 	int	screen_x;
-// 	int	screen_y;
-//
-// 	screen_x = (vars->map->monster_x * vars->unit_size) + vars->unit_size / 2;
-// 	screen_y = (vars->map->monster_y * vars->unit_size) + vars->unit_size / 2;
-// 	vars->animated_sprite->current_frame_ptr = 
-// 		vars->animated_sprite->frames[vars->animated_sprite->current_frame];
-// 	mlx_put_image_to_window(vars->mlx->mlx, vars->mlx->win, 
-// 		vars->animated_sprite->current_frame_ptr, screen_x, screen_y);
-// }
-
-int	draw_sprite(t_vars *vars)
-{
-	//update_sprite_frame(vars->animated_sprite);
-	draw_map(vars);
-	mlx_put_image_to_window(vars->mlx->mlx, vars->mlx->win,
-		vars->image->mlx_img, 0, 0);
-	return (0);
 }
 
 void	calculate_sprite_distances(t_vars *vars)
@@ -130,63 +108,15 @@ void	sort_sprites(t_vars *vars)
 		}
 	}
 }
-
-void	draw_sprites(t_vars *vars)
-{
-	int	i;
-	int	draw_start_y;
-	int	draw_end_y;
-	int	draw_start_x;
-	int	draw_end_x;
-	int	stripe;
-	int	y;
-	int	tex_x;
-	int	tex_y;
-	int	color;
-
-	calculate_sprite_distances(vars);
-	sort_sprites(vars);
-	i = 0;
-	while (i < vars->num_sprites)
-	{
-		project_sprite(vars, &vars->sprites[i]);
-		draw_start_y = -vars->sprites[i].height / 2 \
-			+ vars->mlx->window_height / 2;
-		if (draw_start_y < 0)
-			draw_start_y = 0;
-		draw_end_y = vars->sprites[i].height / 2 + vars->mlx->window_height / 2;
-		if (draw_end_y >= vars->mlx->window_height)
-			draw_end_y = vars->mlx->window_height - 1;
-		draw_start_x = -vars->sprites[i].width / 2 + vars->sprites[i].screen_x;
-		if (draw_start_x < 0)
-			draw_start_x = 0;
-		draw_end_x = vars->sprites[i].width / 2 + vars->sprites[i].screen_x;
-		if (draw_end_x >= vars->mlx->window_width)
-			draw_end_x = vars->mlx->window_width - 1;
-		stripe = draw_start_x;
-		while (stripe < draw_end_x)
-		{
-			if (vars->sprites[i].distance < vars->zbuffer[stripe])
-			{
-				y = draw_start_y;
-				while (y < draw_end_y)
-				{
-					tex_x = (int)((stripe - (-vars->sprites[i].width / 2 \
-						+ vars->sprites[i].screen_x)) \
-						* vars->sprite_texture->width / vars->sprites[i].width);
-					tex_y = (int)((y - (-vars->sprites[i].height / 2 \
-						+ vars->mlx->window_height / 2)) \
-						* vars->sprite_texture->height \
-						/ vars->sprites[i].height);
-					color = get_texture_color(vars->sprite_texture, \
-						tex_x, tex_y);
-					if (color != -1)
-						put_pixel_to_image(vars, stripe, y, color);
-					y++;
-				}
-			}
-			stripe++;
-		}
-		i++;
-	}
-}
+// void	put_enemy_on_screen(t_vars *vars)
+// {
+// 	int	screen_x;
+// 	int	screen_y;
+//
+// 	screen_x = (vars->map->monster_x * vars->unit_size) + vars->unit_size / 2;
+// 	screen_y = (vars->map->monster_y * vars->unit_size) + vars->unit_size / 2;
+// 	vars->animated_sprite->current_frame_ptr = 
+// 		vars->animated_sprite->frames[vars->animated_sprite->current_frame];
+// 	mlx_put_image_to_window(vars->mlx->mlx, vars->mlx->win, 
+// 		vars->animated_sprite->current_frame_ptr, screen_x, screen_y);
+// }
