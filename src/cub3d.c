ac378d0843d1b313a7dbbacf819e8ede8c61e30f
Andrey Arama <anarama@c2r7p4.42vienna.com>
2024-09-11 19:59:54 +0200

    fixed freeze and raycasting mistake

--- src/cub3d.c
+++ src/cub3d.c
@@ -17,10 +17,11 @@ int	main_loop_hook(t_vars *vars)
 	struct timeval	t;
 	double			abc;
 
+	reset_mouse_to_center(vars);
 	get_current_time(&t);
 	abc = (double)t.tv_sec + (double)t.tv_usec / 1000000;
 	get_current_time(&vars->player->time1);
-	//handle_player_damaged_time(vars);
+	// handle_player_damaged_time(vars);
 	update_enemy_list(vars->imp_list, 300, vars->map->imp_list_size);
 	update_enemy_list(vars->caco_list, 300, vars->map->caco_list_size);
 	search_for_player(vars);
