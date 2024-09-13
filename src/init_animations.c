874d2893d31c6291ddd0d350cbf97f46d6eae0a7
Istvan Beregszaszi <iberegsz@c2r7p5.42vienna.com>
2024-09-13 14:33:03 +0200

    Format init_imp_sprite and init_caco_sprite

--- src/init_animations.c
+++ src/init_animations.c
@@ -6,7 +6,7 @@
 /*   By: iberegsz <iberegsz@student.42vienna.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/11 14:14:05 by anarama           #+#    #+#             */
-/*   Updated: 2024/09/12 17:33:03 by iberegsz         ###   ########.fr       */
+/*   Updated: 2024/09/13 14:31:08 by iberegsz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,36 +14,17 @@
 
 void	init_imp_sprite(t_vars *vars, t_animation *imp_animation)
 {
-	const char *imp_move_frames[] \
-	= {
-		"./assets/imp_walk1.xpm",
-		"./assets/imp_walk2.xpm",
-		"./assets/imp_walk3.xpm",
-		"./assets/imp_walk4.xpm",
-		NULL
-	};
-	const char *imp_death_frames[] \
-	= {
-		"./assets/imp_dies1.xpm",
-		"./assets/imp_dies2.xpm",
-		"./assets/imp_dies3.xpm",
-		"./assets/imp_dies4.xpm",
-		"./assets/imp_dies5.xpm",
-		NULL
-	};
-	const char *imp_attack_frames[] \
-	= {
-		"./assets/imp_attack1.xpm",
-		"./assets/imp_attack2.xpm",
-		"./assets/imp_attack3.xpm",
-		NULL
-	};
-	const char *imp_fire_ball_frames[] \
-	= {
-		"./assets/imp_fire1.xpm",
-		"./assets/imp_fire2.xpm",
-		NULL
-	};
+	const char	*imp_move_frames[] = {"./assets/imp_walk1.xpm", \
+		"./assets/imp_walk2.xpm", "./assets/imp_walk3.xpm",
+		"./assets/imp_walk4.xpm", NULL};
+	const char	*imp_death_frames[] = {"./assets/imp_dies1.xpm", \
+		"./assets/imp_dies2.xpm", "./assets/imp_dies3.xpm", \
+		"./assets/imp_dies4.xpm", "./assets/imp_dies5.xpm",	NULL};
+	const char	*imp_attack_frames[] = {"./assets/imp_attack1.xpm", \
+		"./assets/imp_attack2.xpm",	"./assets/imp_attack3.xpm",	NULL};
+	const char	*imp_fire_ball_frames[] = {"./assets/imp_fire1.xpm", \
+		"./assets/imp_fire2.xpm", NULL};
+
 	initialise_enemy_textures(vars, imp_animation->move, imp_move_frames);
 	initialise_enemy_textures(vars, imp_animation->death, imp_death_frames);
 	initialise_enemy_textures(vars, imp_animation->attack, imp_attack_frames);
@@ -53,38 +34,17 @@ void	init_imp_sprite(t_vars *vars, t_animation *imp_animation)
 
 void	init_caco_sprite(t_vars *vars, t_animation *caco_animation)
 {
-	const char *caco_move_frames[] \
-	= {
-		"./assets/cacodemon1.xpm",
-		"./assets/caco_walk1.xpm",
-		"./assets/caco_walk2.xpm",
-		"./assets/caco_walk3.xpm",
-		"./assets/caco_walk4.xpm",
-		"./assets/caco_walk5.xpm",
-		"./assets/caco_walk6.xpm",
-		NULL
-	};
-	const char *caco_death_frames[] \
-	= {
-		"./assets/caco_dies1.xpm",
-		"./assets/caco_dies2.xpm",
-		"./assets/caco_dies3.xpm",
-		"./assets/caco_dies4.xpm",
-		"./assets/caco_dies5.xpm",
-		NULL
-	};
-	const char *caco_attack_frames[] \
-	= {
-		"./assets/cacodemon2.xpm",
-		"./assets/cacodemon3.xpm",
-		"./assets/cacodemon4.xpm",
-		NULL
-	};
-	const char *caco_fire_ball_frames[] \
-	= {
-		"./assets/caco_fire1.xpm",
-		NULL
-	};
+	const char	*caco_move_frames[] = {"./assets/cacodemon1.xpm", \
+		"./assets/caco_walk1.xpm", "./assets/caco_walk2.xpm", \
+		"./assets/caco_walk3.xpm", "./assets/caco_walk4.xpm", \
+		"./assets/caco_walk5.xpm", "./assets/caco_walk6.xpm", NULL};
+	const char	*caco_death_frames[] = {"./assets/caco_dies1.xpm", \
+		"./assets/caco_dies2.xpm", "./assets/caco_dies3.xpm", \
+		"./assets/caco_dies4.xpm", "./assets/caco_dies5.xpm", NULL};
+	const char	*caco_attack_frames[] = {"./assets/cacodemon2.xpm", \
+		"./assets/cacodemon3.xpm", "./assets/cacodemon4.xpm", NULL};
+	const char	*caco_fire_ball_frames[] = {"./assets/caco_fire1.xpm", NULL};
+
 	initialise_enemy_textures(vars, caco_animation->move, caco_move_frames);
 	initialise_enemy_textures(vars, caco_animation->death, caco_death_frames);
 	initialise_enemy_textures(vars, caco_animation->attack, caco_attack_frames);
