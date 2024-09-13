5cb2fee7da2ba923f1634f65bd485a4b8c2b9cf8
Istvan Beregszaszi <iberegsz@c2r7p5.42vienna.com>
2024-09-14 00:43:59 +0200

    Protecting the get_texture_color, but needs to be fixed - still buggy looking sprite

--- src/map_utils.c
+++ src/map_utils.c
@@ -6,7 +6,7 @@
 /*   By: iberegsz <iberegsz@student.42vienna.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/21 22:40:10 by andrejarama       #+#    #+#             */
-/*   Updated: 2024/09/12 13:42:43 by iberegsz         ###   ########.fr       */
+/*   Updated: 2024/09/14 00:36:26 by iberegsz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,10 +50,20 @@ int	get_texture_color(t_img *texture, int x, int y)
 	int	index;
 	int	color;
 
+	if (!texture || !texture->addr)
+	{
+		perror("Error: Null texture or texture address\n");
+		return (-1);
+	}
+	if (x < 0 || x >= texture->width || y < 0 || y >= texture->height)
+	{
+		perror("Error: Texture coordinates out of bounds\n");
+		return (-1);
+	}
 	index = (x * (texture->bits_per_pixel / 8)) + (y * texture->line_len);
 	color = (texture->addr[index] & 0xFF) \
-			| ((texture->addr[index + 1] & 0xFF) << 8) \
-			| ((texture->addr[index + 2] & 0xFF) << 16);
+		| ((texture->addr[index + 1] & 0xFF) << 8) \
+		| ((texture->addr[index + 2] & 0xFF) << 16);
 	if ((color & 0x00FFFFFF) == TRANSPARENT)
 		return (-1);
 	return (color);
