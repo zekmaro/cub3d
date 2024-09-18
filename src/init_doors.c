1ba8bd52828357f7fb4511d6a83dedfe9f15db3d
Istvan Beregszaszi <iberegsz@c2r7p5.42vienna.com>
2024-09-18 21:48:11 +0200

    Fixing memory issues after scan build

--- src/init_doors.c
+++ src/init_doors.c
@@ -6,7 +6,7 @@
 /*   By: iberegsz <iberegsz@student.42vienna.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/14 14:35:24 by iberegsz          #+#    #+#             */
-/*   Updated: 2024/09/14 20:06:34 by iberegsz         ###   ########.fr       */
+/*   Updated: 2024/09/18 21:47:27 by iberegsz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,7 +49,7 @@ void	init_doors(t_vars *vars)
 
 	counter_doors = 0;
 	i = -1;
-	while (vars->map->grid[++i])
+	while (vars->map->gridgi[++i])
 	{
 		j = -1;
 		while (vars->map->grid[i][++j])
